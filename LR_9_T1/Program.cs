a9eedd7388db0e2ff9d93b0d70f44218b5904db3
Roman <asssfsa010@gmail.com>
2025-01-23 21:47:23 +0200

    bratishka

--- LR_9_T1/Program.cs
+++ LR_9_T1/Program.cs
@@ -10,7 +10,6 @@ namespace LR_9_T1
     {
         static void Main(string[] args)
         {
-            // Варіант (а)
             double x, y, xStart, xEnd, step;
 
             Console.WriteLine("Введіть x початкове: ");
@@ -27,23 +26,6 @@ namespace LR_9_T1
                 Console.WriteLine("{0:F2}\t\t{1:F4}", x, y);
                 x += step;
             }
-
-            // Варіант (б)
-            //Console.WriteLine("\nВаріант (б):");
-            //xStart = 2.5;
-            //dx = 1.5;
-            //int n = 5; // кількість ітерацій
-            //x = xStart;
-            //int iteration = 0;
-            //Console.WriteLine("x\t\tY");
-            //while (iteration < n)
-            //{
-            //    double y = (Math.Pow(x, 2) - 3) / (3 * Math.Log(x));
-            //    Console.WriteLine("{0:F2}\t\t{1:F4}", x, y);
-            //    x += dx;
-            //    iteration++;
-            //}
-
             Console.ReadKey();
         }
     }
--- LR_9_T2/Program.cs
+++ LR_9_T2/Program.cs
@@ -1,16 +1,86 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LR_9_T2
 {
-    internal class Program
+    class Program
     {
+        static bool inputInt(ref int i, string povidom)
+        {
+            string s;
+            s = i.ToString();
+        povtor:
+            s = Interaction.InputBox(povidom, "Введення", s);
+            try
+            {
+                i = Convert.ToInt32(s);
+            }
+            catch (System.FormatException)
+            {
+                if (MessageBox.Show("Ви ввели не ціле число." + Strings.Chr(13) + "Бажаєте повторити?", "Увага",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    goto povtor;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+
+        static bool inputDouble(ref double x, string povidom)
+        {
+            string s;
+            s = x.ToString();
+        povtor:
+            s = Interaction.InputBox(povidom, "Введення", s);
+            try
+            {
+                x = Convert.ToDouble(s);
+            }
+            catch (System.FormatException)
+            {
+                if (MessageBox.Show("Ви ввели не число." + Strings.Chr(13) + "Бажаєте повторити?", "Увага",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    goto povtor;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+
         static void Main(string[] args)
         {
+            int n, i;
+            double x, y, xp, dx;
+            xp = 2.5;
+            dx = 1.5;
+            n = 5;
+            if (!inputDouble(ref xp, "Введіть x початкове:"))
+                return;
+            if (!inputDouble(ref dx, "Введіть крок:"))
+                return;
+            if (!inputInt(ref n, "Введіть кількість точок:"))
+                return;
 
+            string rez = "x\t\ty\n";
+            x = xp;
+            i = 0;
+            while (i < n)
+            {
+                y = (x + Math.Cos(2 * x)) / (3 * x);
+                rez += x.ToString() + "\t" + y.ToString() + "\n";
+                x += dx;
+                i++;
+            }
+            MessageBox.Show(rez, "Результати табулювання");
         }
     }
+
+
 }
