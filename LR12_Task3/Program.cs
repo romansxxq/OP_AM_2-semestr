3bcafba5148c7069b175cdce57149dc35a52da89
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-04-05 01:32:11 +0300

    виконано 3 задачу для лр №12

---
+++ LR12_Task3/Program.cs
@@ -0,0 +1,101 @@
+﻿//using Microsoft.VisualBasic;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace LR12_Task3
+//{
+//    internal class Program
+//    {
+//        static List<int> FindPositions(int[] arr, int target)
+//        {
+//            List<int> pos = new List<int>();
+//            for (int i = 0; i < arr.Length; i++)
+//            {
+//                if (arr[i] == target)
+//                    pos.Add(i);
+//            }
+//            return pos;
+//        }
+//        static bool inputInt(ref int num, string mes, string s)
+//        {
+//            string input = Interaction.InputBox(mes, "Введення", s);
+//            return int.TryParse(input, out num);
+//        }
+//        static void ShowMessage(string message, string title, MessageBoxIcon icon)
+//        {
+//            MessageBox.Show(message, title, MessageBoxButtons.OK, icon);
+//        }
+//        static void Main(string[] args)
+//        {
+//            int[] arr = { 1, 2, 5, 5, 10, 9, 30 };
+//            int target = 0;
+
+//            if (inputInt(ref target, "Введіть число для пошуку:", "5"))
+//            {
+//                List<int> pos = FindPositions(arr, target);
+
+//                if (pos.Count > 0)
+//                    MessageBox.Show($"Число {target} знайдено на позиціях: {string.Join(", ", pos)}", "Результати", MessageBoxButtons.OK, MessageBoxIcon.Information);
+//                else
+//                    MessageBox.Show($"Число {target} не знайдено в масиві.", "Результати", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+//            }
+//            else
+//                MessageBox.Show($"Число {target} не знайдено в масиві.", "Результати", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//        }
+//    }
+//}
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR12_Task3
+{
+    internal class Program
+    {
+        static List<int> FindPositions(int[] arr, int target)
+        {
+            List<int> pos = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                    pos.Add(i);
+            }
+            return pos;
+        }
+
+        static bool inputInt(ref int num, string mes, string s)
+        {
+            string input = Interaction.InputBox(mes, "Введення", s);
+            return int.TryParse(input, out num);
+        }
+
+        static void ShowMessage(string message, string title, MessageBoxIcon icon)
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, icon);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 5, 5, 10, 9, 30, 5 };
+            int target = 0;
+
+            bool isValidInput = inputInt(ref target, "Введіть число для пошуку:", "5");
+
+            ShowMessage(isValidInput && target != 0
+                ? (FindPositions(arr, target).Count > 0
+                    ? $"Число {target} знайдено на позиціях: {string.Join(", ", FindPositions(arr, target))}"
+                    : $"Число {target} не знайдено в масиві.")
+                : "Невірний ввід. Будь ласка, введіть правильне ціле число.",
+                "Результати",
+                isValidInput && target != 0 ? (FindPositions(arr, target).Count > 0 ? MessageBoxIcon.Information : MessageBoxIcon.Warning) : MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ LR12_Task3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LR12_Task3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LR12_Task3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f071c363-b2d1-4c21-83b3-af6f4d81cf6c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
