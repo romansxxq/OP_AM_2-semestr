90159ee4c309c5e132355e1329dc21d2933f715d
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-04-19 22:56:42 +0300

    Виконав лр 12 повністю

---
+++ LR12_Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static int[] ReadIntArr(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                bool IsValid = false;
+                while (!IsValid)
+                {
+                    string s = Interaction.InputBox($"Введіть число для елемент {i + 1}:", "Введення", "0");
+                    if (int.TryParse(s, out arr[i]))
+                    {
+                        IsValid = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Некоректне число! Спробуйте ще раз.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            return arr;
+        }
+        static int Res(int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return (int)(2 * Math.Pow(sum, 2));
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = ReadIntArr(7);
+            int res = Res(nums);
+
+            MessageBox.Show($"Подвоєний квадрат суми введених чисел дорівнює: {res}", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
--- LR12_Task3/Program.cs
+++ LR12_Task3/Program.cs
@@ -70,7 +70,23 @@ namespace LR12_Task3
             }
             return pos;
         }
+        //static int BinarySearch(int[] arr, int target)
+        //{
+        //    int left = 0;
+        //    int right = arr.Length - 1;
 
+        //    while (left <= right)
+        //    {
+        //        int mid = (left + right) / 2;
+        //        if (arr[mid] == target)
+        //            return mid;
+        //        else if (arr[mid] < target)
+        //            left = mid + 1;
+        //        else 
+        //            right = mid - 1;
+        //    }
+        //    return -1;
+        //}
         static bool inputInt(ref int num, string mes, string s)
         {
             string input = Interaction.InputBox(mes, "Введення", s);
